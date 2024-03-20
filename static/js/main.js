fcc2d583c6e9b8d024239e310f10524ddcb93f9a
Baristasbouquet <baristasbouquet@gmail.com>
2024-03-20 15:22:10 -0500

    added favicon and tweaked padding and map sizing

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,6 @@
-const mapFrame = document.getElementById('map-frame')
-mapFrame.height = `${Math.round((mapFrame.offsetWidth * 0.75))}`
+const mapFrame = document.getElementById('map-frame');
+let mapFrameHeight = Math.round((mapFrame.offsetWidth * 0.75));
+if (mapFrameHeight < 400) {
+    mapFrameHeight = 400
+};
+mapFrame.height = `${mapFrameHeight}`
