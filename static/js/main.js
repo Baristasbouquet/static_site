578920210d3d4319291f998ddd20adc7a899a9c1
Brandt-C <brandt.carlson11@gmail.com>
2024-05-18 13:15:21 -0500

    revamped site!

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,10 @@
-const mapFrame = document.getElementById('map-frame');
-let mapFrameHeight = Math.round((mapFrame.offsetWidth * 0.75));
-if (mapFrameHeight < 400) {
-    mapFrameHeight = 400
-};
-mapFrame.height = `${mapFrameHeight}`
+// const mapFrame = document.getElementById('map-frame');
+// let mapFrameHeight = Math.round((mapFrame.offsetWidth * 0.75));
+// if (mapFrameHeight < 400) {
+//     mapFrameHeight = 400
+// };
+// mapFrame.height = `${mapFrameHeight}`
+
+const notOpenModal = new bootstrap.Modal(document.getElementById('notOpenModal'), {}) 
+
+notOpenModal.toggle()
